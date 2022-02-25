630680da8686af19e3091319133f0369ad21fb66
Bhakti777 <bhakti1605@gmail.com>
2022-02-25 23:06:59 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import'./todolist.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ToDoList from './Components/TodoList';
+
+
+function App() {
+  return (
+   <>
+     <ToDoList/>
+   </>
+  );
+}
+
+export default App;
